c6fc37fc839491acf2e899f4b04251c08401e8ca
aereal <aereal@aereal.org>
2020-03-26 23:39:24 +0900

    attach cloudbuild.builds.builder role to cloud build service account

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ provider "google" {
   zone    = "asia-northeast1-a"
 }
 
+data "google_project" "current" {}
+
 resource "google_app_engine_application" "app" {
   project     = "merge-chance-time"
   location_id = "asia-northeast1"
@@ -30,5 +32,14 @@ resource "google_project_iam_binding" "github_actions_cloud_build" {
   role    = "roles/cloudbuild.builds.builder"
   members = [
     "serviceAccount:${google_service_account.github_actions_service_account.email}"
+  ]
+}
+
+// refs. https://cloud.google.com/cloud-build/docs/securing-builds/set-service-account-permissions
+resource "google_project_iam_binding" "cloud_build_service_account" {
+  project = "merge-chance-time"
+  role    = "roles/cloudbuild.builds.builder"
+  members = [
+    "serviceAccount:${data.google_project.current.number}@cloudbuild.gserviceaccount.com"
   ]
 }
