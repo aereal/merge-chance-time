c1a2debb0ab0475facf2b3021712eb4667df62ba
aereal <aereal@aereal.org>
2020-03-31 20:09:35 +0900

    use custom role

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "org-aereal"
+
+    workspaces {
+      name = "merge-chance-time"
+    }
+  }
+}
+
 variable "google_service_account" {}
 
 provider "google" {
@@ -23,15 +34,7 @@ resource "google_service_account" "github_actions_service_account" {
 
 resource "google_project_iam_binding" "github_actions_appengine_admin" {
   project = "merge-chance-time"
-  role    = "roles/appengine.appAdmin"
-  members = [
-    "serviceAccount:${google_service_account.github_actions_service_account.email}"
-  ]
-}
-
-resource "google_project_iam_binding" "github_actions_cloud_build" {
-  project = "merge-chance-time"
-  role    = "roles/cloudbuild.builds.builder"
+  role    = google_project_iam_custom_role.github_actions_executor.role_id
   members = [
     "serviceAccount:${google_service_account.github_actions_service_account.email}"
   ]
@@ -43,5 +46,21 @@ resource "google_project_iam_binding" "cloud_build_service_account" {
   role    = "roles/cloudbuild.builds.builder"
   members = [
     "serviceAccount:${data.google_project.current.number}@cloudbuild.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_custom_role" "github_actions_executor" {
+  role_id = "githubActionsExecutor"
+  title   = "GitHub Actions Executor"
+  permissions = [
+    "appengine.applications.get",
+    "appengine.applications.update",
+    "appengine.operations.get",
+    "appengine.services.list",
+    "appengine.services.update",
+    "appengine.versions.create",
+    "appengine.versions.delete",
+    "appengine.versions.list",
+    "appengine.versions.update",
   ]
 }
