95d5d02995858d878bdfdfdc9f01e8b997211363
aereal <aereal@aereal.org>
2020-04-29 21:08:55 +0900

    admin-front: fix sign-in page layout

--- packages/admin-front/src/pages/SignInPage.tsx
+++ packages/admin-front/src/pages/SignInPage.tsx
@@ -1,15 +1,18 @@
 import React, { FC, FormEvent } from "react"
-import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
 import makeStyles from "@material-ui/core/styles/makeStyles"
 import Avatar from "@material-ui/core/Avatar"
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
+import Typography from "@material-ui/core/Typography"
 import { Helmet } from "react-helmet"
 import { apiOrigin } from "../api-origin"
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
@@ -25,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export const SignInPage: FC = () => {
-  const classes = useStyles()
+  const { paper, avatar, form, submit } = useStyles()
 
   const onSubmit = (event: FormEvent): void => {
     event.preventDefault()
@@ -39,18 +42,19 @@ export const SignInPage: FC = () => {
       <Helmet>
         <title>Sign In - Merge Chance Time</title>
       </Helmet>
-      <Grid item xs={12}>
-        <div className={classes.paper}>
-          <Avatar className={classes.paper}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <form className={classes.form} noValidate onSubmit={onSubmit}>
-            <Button className={classes.submit} type="submit" fullWidth variant="contained" color="primary">
-              Sign in with GitHub
-            </Button>
-          </form>
-        </div>
-      </Grid>
+      <div className={paper}>
+        <Avatar className={avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5" component="h1">
+          Sign in
+        </Typography>
+        <form className={form} noValidate onSubmit={onSubmit}>
+          <Button className={submit} type="submit" fullWidth variant="contained" color="primary">
+            Sign in with GitHub
+          </Button>
+        </form>
+      </div>
     </>
   )
 }
