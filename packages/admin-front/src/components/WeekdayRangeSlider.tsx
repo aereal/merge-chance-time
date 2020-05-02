96550b1a5fde76de0231b8149966f0ad0ff46621
aereal <aereal@aereal.org>
2020-05-02 21:14:54 +0900

    admin-front: toggle day

--- packages/admin-front/src/components/WeekdayRangeSlider.tsx
+++ packages/admin-front/src/components/WeekdayRangeSlider.tsx
@@ -1,9 +1,11 @@
 import React, { FC, ChangeEvent } from "react"
 import Slider from "@material-ui/core/Slider"
-import Typography from "@material-ui/core/Typography"
 import red from "@material-ui/core/colors/red"
 import indigo from "@material-ui/core/colors/indigo"
 import makeStyles from "@material-ui/core/styles/makeStyles"
+import Switch from "@material-ui/core/Switch"
+import FormGroup from "@material-ui/core/FormGroup"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
 import { Weekday, sunday, saturday, wholeDay } from "../schedule"
 
 export type OnUpdateValue = (value: number | number[] | null) => void
@@ -27,27 +29,36 @@ const useSyles = makeStyles({
 export const WeekdayRangeSlider: FC<WeekdayRangeSliderProps> = ({ weekday, onUpdateValue, scheduleRange }) => {
   const { sundayLabel, saturdayLabel } = useSyles()
 
+  const available = scheduleRange !== null
   const colors: Partial<Record<Weekday, string>> = {
     [saturday]: saturdayLabel,
     [sunday]: sundayLabel,
   }
-  const handleChange = (_: ChangeEvent<{}>, value: number | number[]): void => {
+  const handleRangeChange = (_: ChangeEvent<{}>, value: number | number[]): void => {
     onUpdateValue(value)
+  }
+  const handleSwitchChange = (_: ChangeEvent<HTMLInputElement>, checked: boolean): void => {
+    onUpdateValue(checked ? scheduleRange ?? wholeDay : null)
   }
 
   return (
     <>
-      <Typography className={colors[weekday]} gutterBottom>
-        {weekday}
-      </Typography>
+      <FormGroup row>
+        <FormControlLabel
+          className={colors[weekday]}
+          label={weekday}
+          control={<Switch color="primary" checked={available} onChange={handleSwitchChange} />}
+        />
+      </FormGroup>
       <Slider
+        disabled={!available}
         marks
         valueLabelDisplay="auto"
         step={1}
         min={wholeDay[0]}
         max={wholeDay[1]}
-        value={scheduleRange ?? wholeDay /* TODO */}
-        onChange={handleChange}
+        value={scheduleRange ?? wholeDay}
+        onChange={handleRangeChange}
       />
     </>
   )
