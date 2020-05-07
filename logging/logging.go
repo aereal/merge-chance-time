47216f289e98b88c8ae3e6e78cfbf1d854200010
aereal <aereal@aereal.org>
2020-05-07 14:33:00 +0900

    app: SetNilLogger for test

--- logging/logging.go
+++ logging/logging.go
@@ -2,7 +2,11 @@ package logging
 
 import (
 	"context"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"reflect"
+	"unsafe"
 
 	stackdriverlog "github.com/yfuruyama/stackdriver-request-context-log"
 )
@@ -24,4 +28,13 @@ func WithLogger(cfg *stackdriverlog.Config) func(http.Handler) http.Handler {
 
 func GetLogger(ctx context.Context) *stackdriverlog.ContextLogger {
 	return ctx.Value(ctxKey).(*stackdriverlog.ContextLogger)
+}
+
+func SetNilLogger(ctx context.Context) context.Context {
+	logger := &stackdriverlog.ContextLogger{}
+	v := reflect.ValueOf(logger).Elem()
+	outv := v.FieldByName("out")
+	ps := (*io.Writer)(unsafe.Pointer(outv.UnsafeAddr()))
+	*ps = ioutil.Discard
+	return context.WithValue(ctx, ctxKey, logger)
 }
